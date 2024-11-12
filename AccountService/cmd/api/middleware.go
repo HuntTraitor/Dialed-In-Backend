107d5a453676fa8e9b461c5da85959c99229b242
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-11-11 17:30:35 -0800

    Added rate limiting on a per ip basis

--- AccountService/cmd/api/middleware.go
+++ AccountService/cmd/api/middleware.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"golang.org/x/time/rate"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -18,14 +21,62 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
-// rateLimit limits the rates of total requests by clients
+// rateLimit limits the rate of requests per client in a map of ips to rates
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	limiter := rate.NewLimiter(2, 4)
+
+	// Each client has their own limiter and lastSeen time
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	// Launch a new goroutine that deletes client ips in the background after a certain amount of time
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !limiter.Allow() {
+
+		// Retrieve ip from host
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		// Assign a new rate limiter to a client if they were not found
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+
+		// Set there last seen time to now
+		clients[ip].lastSeen = time.Now()
+
+		// If the client has exceeded rate limits, send an error and unlock the mutex
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// Unlock mutex and continue
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
+
 }
