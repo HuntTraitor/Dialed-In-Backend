293ba9b623c4e95c2c2fca60061c65d4d8c80764
Hunter Tratar <hunterrrisatratar@gmail.com>
2024-11-23 10:59:55 -0800

    e2e tests for activating a user

--- e2e/testutils_test.go
+++ e2e/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 	"testing"
 	"time"
 )
@@ -34,7 +35,34 @@ func post(t *testing.T, url string, body io.Reader) (int, http.Header, map[strin
 	return res.StatusCode, res.Header, responseBody
 }
 
+// put takes a URL and body and returns a status code, headers, and a JSON body
+func put(t *testing.T, url string, body io.Reader) (int, http.Header, map[string]any) {
+	req, err := http.NewRequest(http.MethodPut, url, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close() // Ensure the response body is closed
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var responseBody map[string]any
+	err = json.Unmarshal(resBody, &responseBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return res.StatusCode, res.Header, responseBody
+}
+
 // getEmail returns the string version and amount of emails with a kind and query linked to
+// and returns the body and number of emails
 // https://github.com/mailhog/MailHog/blob/master/docs/APIv2/swagger-2.0.yaml
 func getEmail(t *testing.T, kind string, query string) (string, int) {
 	t.Helper()
@@ -79,4 +107,19 @@ func waitFor(t *testing.T, condition func() bool) {
 			}
 		}
 	}
+}
+
+// extractToken takes an email body from mailhog and returns the token inside of that body
+func extractToken(t *testing.T, emailContent string) string {
+	t.Helper()
+	re := regexp.MustCompile(`\\?"token\\?":\\?\s*\\?"([A-Z0-9]+)\\?"`)
+	match := re.FindStringSubmatch(emailContent)
+
+	if len(match) > 1 {
+		// The token is in the first capturing group
+		return match[1]
+	} else {
+		t.Fatal("failed to extract token")
+	}
+	return ""
 }
--- e2e/users_test.go
+++ e2e/users_test.go
@@ -137,3 +137,96 @@ func TestCreateUser(t *testing.T) {
 		})
 	}
 }
+
+func TestActivateUser(t *testing.T) {
+	cleanup, _, err := LaunchTestProgram(port)
+	if err != nil {
+		t.Fatalf("failed to launch test program: %v", err)
+	}
+	t.Cleanup(cleanup)
+
+	tests := []struct {
+		name               string
+		setupPayload       func(token string) string
+		expectedStatusCode int
+		expectedWrapper    string
+		expectedResponse   map[string]any
+	}{
+		{
+			name: "Successfully activate user",
+			setupPayload: func(token string) string {
+				return fmt.Sprintf(`{"token":"%s"}`, token)
+			},
+			expectedStatusCode: http.StatusOK,
+			expectedWrapper:    "user",
+			expectedResponse: map[string]any{
+				"user": map[string]any{
+					"activated": true,
+				},
+			},
+		},
+		{
+			name: "User did not input a token",
+			setupPayload: func(token string) string {
+				return `{"token":""}`
+			},
+			expectedStatusCode: http.StatusUnprocessableEntity,
+			expectedWrapper:    "error",
+			expectedResponse: map[string]any{
+				"error": map[string]any{
+					"token": "must be provided",
+				},
+			},
+		},
+		{
+			name: "Token is incorrect / expired",
+			setupPayload: func(token string) string {
+				return fmt.Sprintf(`{"token":"ASDJKLEPOIURERFJDKSLAIEJG1"}`)
+			},
+			expectedStatusCode: http.StatusUnprocessableEntity,
+			expectedWrapper:    "error",
+			expectedResponse: map[string]any{
+				"error": map[string]any{
+					"token": "token not found",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a new user
+			payload := `{
+				"name":     "Test User",
+				"email":    "test@example.com",
+				"password": "password"
+			}`
+			requestURL := fmt.Sprintf("http://localhost:%d/v1/users", 3001)
+			_, _, _ = post(t, requestURL, strings.NewReader(payload))
+
+			// Fetch activation token from email
+			var token string
+			waitFor(t, func() bool {
+				body, _ := getEmail(t, "containing", "token")
+				token = extractToken(t, body)
+				return token != ""
+			})
+
+			// Activate the user
+			requestURL = fmt.Sprintf("http://localhost:%d/v1/users/activated", 3001)
+			payload = tt.setupPayload(token)
+			statusCode, _, body := put(t, requestURL, strings.NewReader(payload))
+
+			// Assertions
+			assert.Equal(t, tt.expectedStatusCode, statusCode)
+			assert.NotEmpty(t, body[tt.expectedWrapper])
+
+			actualContent := body[tt.expectedWrapper].(map[string]any)
+			expectedContent := tt.expectedResponse[tt.expectedWrapper].(map[string]any)
+
+			for key, value := range expectedContent {
+				assert.Equal(t, value, actualContent[key], "Mismatch for key: %s", key)
+			}
+		})
+	}
+}
