4f88519fcabbd2c81b1f4979038c4cc4311e4723
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-02-08 19:20:17 -0800

    Created a uploadToS3 function

---
+++ internal/s3/s3.go
@@ -0,0 +1,19 @@
+package s3
+
+import (
+	"bytes"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+)
+
+// uploadToS3 takes a client, file, filename, and bucket and uploads a file to that s3 bucket
+func uploadToS3(client s3iface.S3API, file []byte, filename string, bucket string) error {
+	_, err := client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   bytes.NewReader(file),
+		ACL:    aws.String("public-read"),
+	})
+	return err
+}
