2ca02e7f7d5e53d40f1770416b4b49d2435fa97d
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-02-21 14:09:09 -0800

    Removed new invalid seeds

--- db/sql/seeds.sql
+++ db/sql/seeds.sql
@@ -5,33 +5,30 @@ INSERT INTO methods (name, img) VALUES
     ('Pour Over', 'https://dialedin.s3.us-east-2.amazonaws.com/methods/pour_over.webp'),
     ('Hario Switch', 'https://dialedin.s3.us-east-2.amazonaws.com/methods/hario_switch.webp');
 
-INSERT INTO coffees(user_id, name, region, process, img, description) VALUES
-    ((SELECT id FROM users WHERE email = 'hunter@gmail.com'), 'Milky Cake', 'Columbia', 'Thermal Shock', 'https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto,c_scale,w_500//Products/Thumbs/limeball_nbz7sk', 'Very sweet coffee with notes of cinnamon');
-
-INSERT INTO recipes(user_id, coffee_id, method_id, info) VALUES
-    (
-     (SELECT id FROM users WHERE email = 'hunter@gmail.com'),
-     (SELECT id FROM coffees WHERE name = 'Milky Cake'),
-     (SELECT id FROM methods WHERE name = 'Hario Switch'),
-     json_build_object(
-        'grams_in', '20',
-        'grams_out', '320',
-        'phase', json_build_object(
-            '1', json_build_object(
-                 'open', 'true',
-                 'time', '45',
-                 'amount', '160'
-                 ),
-            '2', json_build_object(
-                 'open', 'false',
-                 'time', '75',
-                 'amount', '160'
-                 ),
-            '3', json_build_object(
-                 'open', 'true',
-                 'time', '60',
-                 'amount', '0'
-                 )
-        )
-     )
-    );
+-- INSERT INTO recipes(user_id, coffee_id, method_id, info) VALUES
+--     (
+--      (SELECT id FROM users WHERE email = 'hunter@gmail.com'),
+--      (SELECT id FROM coffees WHERE name = 'Milky Cake'),
+--      (SELECT id FROM methods WHERE name = 'Hario Switch'),
+--      json_build_object(
+--         'grams_in', '20',
+--         'grams_out', '320',
+--         'phase', json_build_object(
+--             '1', json_build_object(
+--                  'open', 'true',
+--                  'time', '45',
+--                  'amount', '160'
+--                  ),
+--             '2', json_build_object(
+--                  'open', 'false',
+--                  'time', '75',
+--                  'amount', '160'
+--                  ),
+--             '3', json_build_object(
+--                  'open', 'true',
+--                  'time', '60',
+--                  'amount', '0'
+--                  )
+--         )
+--      )
+--     );
--- db/sql/test_setup.sql
+++ db/sql/test_setup.sql
@@ -50,33 +50,30 @@ INSERT INTO methods (name, img) VALUES
                                     ('Pour Over', 'https://www.lacolombe.com/cdn/shop/products/BrewMerch_Holiday2238166_2.jpg?v=1736688225'),
                                     ('Hario Switch', 'https://www.hario-canada.ca/cdn/shop/products/hario_ssd-200-b-v60-02-switch-immersion-dripper_pouring_1024x1024.jpg?v=1672852353');
 
-INSERT INTO coffees(user_id, name, region, img, description) VALUES
-    ((SELECT id FROM users WHERE email = 'hunter@gmail.com'), 'Milky Cake', 'Columbia', 'https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto,c_scale,w_500//Products/Thumbs/limeball_nbz7sk', 'Very sweet coffee with notes of cinnamon');
-
-INSERT INTO recipes(user_id, coffee_id, method_id, info) VALUES
-    (
-        (SELECT id FROM users WHERE email = 'hunter@gmail.com'),
-        (SELECT id FROM coffees WHERE name = 'Milky Cake'),
-        (SELECT id FROM methods WHERE name = 'Hario Switch'),
-        json_build_object(
-                'grams_in', '20',
-                'grams_out', '320',
-                'phase', json_build_object(
-                        '1', json_build_object(
-                        'open', 'true',
-                        'time', '45',
-                        'amount', '160'
-                             ),
-                        '2', json_build_object(
-                                'open', 'false',
-                                'time', '75',
-                                'amount', '160'
-                             ),
-                        '3', json_build_object(
-                                'open', 'true',
-                                'time', '60',
-                                'amount', '0'
-                             )
-                         )
-        )
-    );
+-- INSERT INTO recipes(user_id, coffee_id, method_id, info) VALUES
+--     (
+--         (SELECT id FROM users WHERE email = 'hunter@gmail.com'),
+--         (SELECT id FROM coffees WHERE name = 'Milky Cake'),
+--         (SELECT id FROM methods WHERE name = 'Hario Switch'),
+--         json_build_object(
+--                 'grams_in', '20',
+--                 'grams_out', '320',
+--                 'phase', json_build_object(
+--                         '1', json_build_object(
+--                         'open', 'true',
+--                         'time', '45',
+--                         'amount', '160'
+--                              ),
+--                         '2', json_build_object(
+--                                 'open', 'false',
+--                                 'time', '75',
+--                                 'amount', '160'
+--                              ),
+--                         '3', json_build_object(
+--                                 'open', 'true',
+--                                 'time', '60',
+--                                 'amount', '0'
+--                              )
+--                          )
+--         )
+--     );
