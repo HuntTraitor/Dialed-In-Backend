1da2357b5647a010837265596a97f1c2e3b9f48e
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-04-03 11:27:46 -0700

    Created type signature for hario switch recipe

---
+++ internal/data/recipes.go
@@ -0,0 +1,22 @@
+package data
+
+type Recipe struct {
+	ID       int        `json:"id"`
+	UserID   int        `json:"user_id"`
+	MethodID int        `json:"method_id"`
+	CoffeeID int        `json:"coffee_id"`
+	Info     RecipeInfo `json:"info"`
+}
+
+type RecipeInfo struct {
+	Name   string  `json:"name"`
+	GramIn int     `json:"grams_in"`
+	MlOut  int     `json:"ml_out"`
+	Phases []Phase `json:"phases"`
+}
+
+type Phase struct {
+	Open   bool `json:"open"`
+	Time   int  `json:"time"`
+	Amount int  `json:"amount"`
+}
