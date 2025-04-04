a8af20225aef0b6b79f12e15228b9ca056746de7
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-04-04 09:50:54 -0700

    Added form validation for hario switch recipes

--- internal/data/recipes.go
+++ internal/data/recipes.go
@@ -3,6 +3,7 @@ package data
 import (
 	"context"
 	"database/sql"
+	"github.com/hunttraitor/dialed-in-backend/internal/validator"
 	"time"
 )
 
@@ -31,6 +32,25 @@ type Phase struct {
 
 type RecipeModel struct {
 	DB *sql.DB
+}
+
+// ValidateRecipe validates a specific recipe is correct
+func ValidateRecipe(v *validator.Validator, recipe *Recipe) {
+	v.Check(recipe.Info.Name != "", "name", "must be provided")
+	v.Check(len(recipe.Info.Name) <= 100, "name", "must not be more than 100 bytes")
+	v.Check(recipe.Info.MlOut > 0, "ml_out", "must be greater than zero")
+	v.Check(recipe.Info.GramIn > 0, "grams_in", "must be greater than zero")
+	v.Check(recipe.Info.MlOut < 1000, "ml_out", "must be less than a thousand")
+	v.Check(recipe.Info.GramIn < 10000, "grams_in", "must be less than ten thousand")
+	for _, phase := range recipe.Info.Phases {
+		ValidatePhase(v, &phase)
+	}
+}
+
+// ValidatePhase validates a phase is the correct format
+func ValidatePhase(v *validator.Validator, phase *Phase) {
+	v.Check(phase.Time > 0, "time", "must be greater than zero")
+	v.Check(phase.Amount > 0, "amount", "must be greater than zero")
 }
 
 type RecipeModelInterface interface {
