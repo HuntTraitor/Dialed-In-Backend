e8149e1eb8a6c867b42e78b26c92e09cc5cf6f20
Hunter Tratar <hunterrrisatratar@gmail.com>
2025-08-17 23:29:43 -0700

    Attached route to server

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -81,4 +81,5 @@ func (app *application) loadCoffeeRoutes(router chi.Router) {
 func (app *application) loadRecipeRoutes(router chi.Router) {
 	router.With(app.requireAuthenticatedUser).Post("/", app.createRecipeHandler)
 	router.With(app.requireAuthenticatedUser).Get("/", app.listRecipesHandler)
+	router.With(app.requireAuthenticatedUser).Patch("/{id}", app.updateRecipeHandler)
 }
